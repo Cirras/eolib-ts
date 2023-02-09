0de35ade89d83676fc37a8b9802aa4209558c844
Jonah Jeleniewski <cirrasbu@hotmail.com>
2023-02-09 21:08:25 +1100

    Add `SerializationError` test

---
+++ test/protocol/serialization-error.test.ts
@@ -0,0 +1,10 @@
+import { SerializationError } from "@eolib/index";
+
+describe("SerializationError", () => {
+  describe("#message", () => {
+    it("should return the message provided at construction", () => {
+      const message = "Oh no, the sun exploded!";
+      expect(new SerializationError(message).message).toStrictEqual(message);
+    });
+  });
+});
